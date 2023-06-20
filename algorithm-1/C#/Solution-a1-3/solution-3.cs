aa148ee68db243433076bc3f5650281588783393
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-06-20 18:27:56 +0300

    Algorithm strucure is built up

--- algorithm-1/C#/Solution-a1-3/solution-3.cs
+++ algorithm-1/C#/Solution-a1-3/solution-3.cs
@@ -1,1 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+[Flags]
+enum Movements
+{
+    None,
+    Top,
+    Right,
+    Bottom,
+    Left,
+}
+class GameState
+{
+    int[][] _values;
+    int[] _addressOfVoid = new int[2];
+    Movements _lastMovement;
+    Movements _movements;
+    int rowNumber = 2;
+    int colNumber = 3;
+    public GameState(int[][] initialState)
+    {
+        if (!ValidateInput(initialState))
+        {
+            Console.WriteLine("Invalid game state as a starting point!");
+            return;
+        }
+        _values = initialState;
+        _getAddressOfVoid();
+    }
+    public void Start()
+    {
+        SetAvailableMovements();
+    }
+    private int _next()
+    {
+
+    }
+    private bool _isSolved()
+    {
+        for(int i = 0; i < rowNumber; i++)
+        {
+            for(int j = 0; j < colNumber - 1; j++)
+            {
+                bool isEqual = _values[i][j] == i *
+                if ()
+            }
+        }
+    }
+    private bool ValidateInput(int[][] initialState)
+    {
+        return true;
+    }
+    private void SetAvailableMovements()
+    {
+        int row = _addressOfVoid[0];
+        int col = _addressOfVoid[1];
+        _movements = Movements.None;
+        if(row - 1 > 0)
+        {
+            _movements = _movements | Movements.Top;
+        }
+        if (row + 1 < _values.Length)
+        {
+            _movements = _movements | Movements.Bottom;
+        }
+        if (col - 1 > 0)
+        {
+            _movements = _movements | Movements.Left;
+        }
+        if (col + 1 < _values[0].Length)
+        {
+            _movements = _movements | Movements.Right;
+        }
+        _movements = _movements ^ _lastMovement; // do not repeat last movement
+    }
+    private void _getAddressOfVoid()
+    {
+        for (int i = 0; i < rowNumber; i++)
+        {
+            for (int j = 0; j < colNumber; j++)
+            {
+                if (_values[i][j] == 0)
+                {
+                    _addressOfVoid[0] = i;
+                    _addressOfVoid[1] = j;
+                }
+            }
+        }
+    }
+}
