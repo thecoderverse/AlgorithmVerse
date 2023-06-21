eb8c0d94f4919b25faf5c5a5b68dab2ce385816f
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-06-21 04:37:00 +0300

    Preliminary solution is added

--- algorithm-1/C#/Solution-a1-3/solution-3.cs
+++ algorithm-1/C#/Solution-a1-3/solution-3.cs
@@ -1,91 +1,322 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-[Flags]
-enum Movements
+using System.Text;
+
+
+Test();
+
+
+static void Test()
 {
-    None,
-    Top,
-    Right,
-    Bottom,
-    Left,
+    Console.WriteLine("==============");
+    Console.WriteLine("Test2");
+    Console.WriteLine("_____");
+    int reqStepCount = 5;
+    Game game_2 = new("[[4,1,2],[5,0,3]]");
+    GameState resolvedState = game_2.GetSolution();
+    Console.WriteLine(resolvedState.ToString());
+    Console.WriteLine(resolvedState.Status.ToString());
+    Console.WriteLine($"resolved in {resolvedState.moveHist.Count} steps");
+    Console.WriteLine($"Test result {(resolvedState.moveHist.Count == reqStepCount ? "passed" : "failed")}.");
 }
-class GameState
+
+
+[Flags]
+public enum Movements
 {
-    int[][] _values;
-    int[] _addressOfVoid = new int[2];
-    Movements _lastMovement;
-    Movements _movements;
-    int rowNumber = 2;
-    int colNumber = 3;
-    public GameState(int[][] initialState)
+    NONE = 0,
+    TOP = 2,
+    RIGHT = 4,
+    BOTTOM = 8,
+    LEFT = 16,
+}
+
+public enum StateStatus
+{
+    IN_PROGRESS,
+    FAILED,
+    SUCCESS
+}
+public class GameState
+{
+    public static int RowNumber = 2;
+    public static int ColNumber = 3;
+    public int[][] Values { get; set; }
+    public int[] AddressOfVoid { get; set; } = new int[2];
+    public Movements LastMovementFrom { get; set; }
+    public List<Movements> moveHist { get; set; }
+    public StateStatus Status { get; set; }
+    public Movements NextMovements { get; set; }
+    /// <summary>
+    /// New GameState by the copy of the initialState. 
+    /// </summary>
+    /// <remarks>
+    /// Constructor copies the initialState nested array into its Values property.
+    /// Constructor also copies movesHistory into moveHist property.
+    /// </remarks>
+    /// <param name="initialState"></param>
+    /// <param name="lastMovementFrom"></param>
+    public GameState(int[][] initialState, Movements lastMovementFrom, List<Movements> movesHistory)
     {
+        Status = StateStatus.IN_PROGRESS;
+        Values = new int[initialState.Length][];
+        LastMovementFrom = lastMovementFrom;
+        moveHist = new();
+        // copy initialState
+        int rowIndex = 0;
+        foreach (int[] row in initialState)
+        {
+            Values[rowIndex] = new int[row.Length];
+            for (int i = 0; i < row.Length; i++)
+            {
+                Values[rowIndex][i] = row[i];
+            }
+            rowIndex++;
+        }
+        foreach (Movements move in movesHistory)
+        {
+            moveHist.Add(move);
+        }
+        _getAddressOfVoid();
+    }
+    public void Move(Movements movement)
+    {
+        int row = AddressOfVoid[0];
+        int col = AddressOfVoid[1];
+        switch (movement)
+        {
+            case Movements.LEFT:
+                Values[row][col] = Values[row][col - 1];
+                Values[row][col - 1] = 0;
+                --AddressOfVoid[1];
+                LastMovementFrom = Movements.RIGHT;
+                break;
+            case Movements.TOP:
+                Values[row][col] = Values[row - 1][col];
+                Values[row - 1][col] = 0;
+                --AddressOfVoid[0];
+                LastMovementFrom = Movements.BOTTOM;
+                break;
+            case Movements.BOTTOM:
+                Values[row][col] = Values[row + 1][col];
+                Values[row + 1][col] = 0;
+                ++AddressOfVoid[0];
+                LastMovementFrom = Movements.TOP;
+                break;
+
+            case Movements.RIGHT:
+                Values[row][col] = Values[row][col + 1];
+                Values[row][col + 1] = 0;
+                ++AddressOfVoid[1];
+                LastMovementFrom = Movements.LEFT;
+                break;
+        }
+        moveHist.Add(movement);
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+        sb.Append("[");
+        bool reapet = false;
+        foreach (int[] row in Values)
+        {
+            if (reapet)
+                sb.Append(",");
+            sb.Append("[");
+            foreach (int val in row)
+            {
+                sb.Append(val);
+            }
+            sb.Append("]");
+        }
+        sb.Append("]");
+        return sb.ToString();
+    }
+
+    public void SetAvailableMovements()
+    {
+        int row = AddressOfVoid[0];
+        int col = AddressOfVoid[1];
+        if (row - 1 >= 0)
+        {
+            NextMovements |= Movements.TOP;
+        }
+        if (row + 1 < RowNumber)
+        {
+            NextMovements |= Movements.BOTTOM;
+        }
+        if (col - 1 >= 0)
+        {
+            NextMovements |= Movements.LEFT;
+        }
+        if (col + 1 < ColNumber)
+        {
+            NextMovements |= Movements.RIGHT;
+        }
+        NextMovements ^= LastMovementFrom; // do not repeat last movement
+    }
+    private void _getAddressOfVoid()
+    {
+        for (int i = 0; i < RowNumber; i++)
+        {
+            for (int j = 0; j < ColNumber; j++)
+            {
+                if (Values[i][j] == 0)
+                {
+                    AddressOfVoid[0] = i;
+                    AddressOfVoid[1] = j;
+                }
+            }
+        }
+    }
+}
+class Game
+{
+    int rowNumber;
+    int colNumber;
+    GameState _initState;
+    public HashSet<string> stateHist { get; set; }
+
+    /// <summary>
+    /// init state as "n,n,n/n,n,n" or "[[n,n,n],[n,n,n]]"
+    /// </summary>
+    /// <param name="initState"></param>
+    public Game(string initState)
+    {
+        Console.WriteLine("New Game:" + initState);
+        int[][] initialState;
+        initState = initState.Replace("[[", "");
+        initState = initState.Replace("]]", "");
+        initState = initState.Replace("],[", "/");
+        string[] rows = initState.Split("/");
+        initialState = new int[rows.Length][];
+        try
+        {
+
+            int i = 0;
+            foreach (string row in rows)
+            {
+                string[] cols = row.Split(",");
+                initialState[i] = new int[cols.Length];
+                int j = 0;
+                foreach (string col in cols)
+                {
+                    initialState[i][j] = int.Parse(col);
+                    j++;
+                }
+                i++;
+            }
+            SetState(initialState);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("<!> Invalid string format for initial state <!>");
+            Console.WriteLine(e.Message);
+        }
+    }
+    public Game(int[][] initialState)
+    {
+        SetState(initialState);
+    }
+    public void SetState(int[][] initialState)
+    {
+        rowNumber = GameState.RowNumber;
+        colNumber = GameState.ColNumber;
         if (!ValidateInput(initialState))
         {
             Console.WriteLine("Invalid game state as a starting point!");
             return;
         }
-        _values = initialState;
-        _getAddressOfVoid();
+        _initState = new GameState(initialState, Movements.NONE, new List<Movements>());
+        stateHist = new();
     }
-    public void Start()
+    public GameState GetSolution()
     {
-        SetAvailableMovements();
+        return GetSolution(_initState);
     }
-    private int _next()
+    private GameState GetSolution(GameState state)
     {
-
-    }
-    private bool _isSolved()
-    {
-        for(int i = 0; i < rowNumber; i++)
+        if (!stateHist.Add(state.ToString()))
         {
-            for(int j = 0; j < colNumber - 1; j++)
+            state.Status = StateStatus.FAILED;
+            return state;
+        }
+        if (_isSolved(state))
+        {
+            state.Status = StateStatus.SUCCESS;
+            return state;
+        }
+        state.SetAvailableMovements();
+        List<GameState> newStates = new();
+        CheckMovement(state, Movements.LEFT, newStates);
+        CheckMovement(state, Movements.TOP, newStates);
+        CheckMovement(state, Movements.RIGHT, newStates);
+        CheckMovement(state, Movements.BOTTOM, newStates);
+        
+
+        GameState resolvedState = state;
+        int count = int.MaxValue;
+        bool successFound = false;
+        foreach (GameState newState in newStates)
+        {
+            GameState newResolvedState = GetSolution(newState);
+            if (successFound == false && newResolvedState.Status == StateStatus.SUCCESS)
             {
-                bool isEqual = _values[i][j] == i *
-                if ()
+                successFound = true;
+                resolvedState = newResolvedState;
+                count = resolvedState.moveHist.Count;
+            }
+            else if (successFound && newResolvedState.Status == StateStatus.SUCCESS)
+            {
+                if (newResolvedState.moveHist.Count < count)
+                {
+                    resolvedState = newResolvedState;
+                    count = resolvedState.moveHist.Count;
+                }
+            }
+            else if(successFound == false)
+            {
+                if (newResolvedState.moveHist.Count < count)
+                {
+                    resolvedState = newResolvedState;
+                    count = resolvedState.moveHist.Count;
+                }
             }
         }
+        return resolvedState;
     }
-    private bool ValidateInput(int[][] initialState)
+    private void CheckMovement(GameState state, Movements movement, List<GameState> newStates)
     {
-        return true;
+        // check availablity
+        bool isMovementAvailable = state.NextMovements.HasFlag(movement);
+        if (isMovementAvailable == false) return;
+        // we return early if this movement is not available
+        GameState _state;
+        _state = new GameState(state.Values, state.LastMovementFrom, state.moveHist);
+        newStates.Add(_state);
+
+        _state.Move(movement);
     }
-    private void SetAvailableMovements()
-    {
-        int row = _addressOfVoid[0];
-        int col = _addressOfVoid[1];
-        _movements = Movements.None;
-        if(row - 1 > 0)
-        {
-            _movements = _movements | Movements.Top;
-        }
-        if (row + 1 < _values.Length)
-        {
-            _movements = _movements | Movements.Bottom;
-        }
-        if (col - 1 > 0)
-        {
-            _movements = _movements | Movements.Left;
-        }
-        if (col + 1 < _values[0].Length)
-        {
-            _movements = _movements | Movements.Right;
-        }
-        _movements = _movements ^ _lastMovement; // do not repeat last movement
-    }
-    private void _getAddressOfVoid()
+    private bool _isSolved(GameState state)
     {
         for (int i = 0; i < rowNumber; i++)
         {
             for (int j = 0; j < colNumber; j++)
             {
-                if (_values[i][j] == 0)
+                int a = state.Values[i][j];
+                int b = (i * colNumber + j + 1) % (rowNumber * colNumber);
+                bool isNotEqual = a != b;
+                if (isNotEqual)
                 {
-                    _addressOfVoid[0] = i;
-                    _addressOfVoid[1] = j;
+                    return false;
                 }
             }
         }
+        return true;
+    }
+    private bool ValidateInput(int[][] initialState)
+    {
+        return true;
     }
 }
